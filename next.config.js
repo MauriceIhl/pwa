0e8ddabd8eee3c84329b5dd43aa0db5a66ce8352
Mihl96 <75532504+Mihl96@users.noreply.github.com>
2021-06-23 11:55:47 +0200

    added new offline fallback

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
  
 module.exports = withPWA({
+    future: {webpack5: true},
     pwa: {
         dest: 'public', 
         runtimeCaching,
+        fallbacks: {
+            document: '/offline',
+          }
     }, 
     images: {
         domains: ['website.mi-portfolio.de']
