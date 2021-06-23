fed0a8cc75a2550595d4819a0d6c60d38f55b574
Mihl96 <75532504+Mihl96@users.noreply.github.com>
2021-06-23 10:56:46 +0200

    added fallback

---
+++ pages/_offline.js
@@ -0,0 +1,18 @@
+import Menu from "../components/DOM_Elements/menu"
+import Footer from "../components/DOM_Elements/footer"
+
+const Offline  = () => {
+
+
+    return (
+        <>
+        <Menu></Menu>
+        <main>
+            Du bist leider Offline ....
+        </main>
+        <Footer></Footer>
+        </>
+    )
+}
+
+export default Offline
